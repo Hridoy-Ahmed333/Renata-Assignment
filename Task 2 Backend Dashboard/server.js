0c869a7236293f64a22ccefc46ffabca332c1947
Hridoy <hridoyahmedf5@gmail.com>
2025-05-20 15:38:58 +0600

    cors issue fixed

--- Task 2 Backend Dashboard/server.js
+++ Task 2 Backend Dashboard/server.js
@@ -7,7 +7,7 @@ const excelRoutes = require("./routes/excelRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
 app.use("/api/data", excelRoutes);
